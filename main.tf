e4fb0f3078180680a2afa842a729370ee1f24557
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2025-05-20 17:26:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,28 +26,6 @@ resource "aws_security_group" "sg_1" {
     env = "prod"
   }
 }
-
-# Security Group 2
-resource "aws_security_group" "sg_2" {
-  name        = "harshit-test-sg-db-server"
-  description = "Security group for database server"
-  vpc_id      = "vpc-0cca2e67"  # Replace with your VPC ID
-
-  ingress {
-    description = "Allow MySQL from SG1"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    security_groups = [aws_security_group.sg_1.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- tags = {
-    env = "dev"
-  }
+removed {
+from = aws_security_group.sg_2
 }
