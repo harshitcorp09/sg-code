e6de9c8e9bb13d9f9831894d31fe4f7cb4694350
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2025-05-20 16:02:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,9 @@ resource "aws_security_group" "sg_1" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ tags = {
+    env = "prod"
+  }
 }
 
 # Security Group 2
@@ -43,5 +46,8 @@ resource "aws_security_group" "sg_2" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+ tags = {
+    env = "dev"
   }
 }
